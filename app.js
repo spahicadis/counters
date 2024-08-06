26d3a80101cc7105e51f3665e68f3c6aa60c8b16
spahicadis <adis@spahica.com>
2024-08-06 23:52:52 +0200

    js dom

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const initalCountValue = document.querySelector('.inital-count-js');
 const maxCountValue = document.querySelector('.max-count-js');
 const addCounterButton = document.querySelector('.button-create-js');
-const createdCountersDiv = document.querySelector('.createdCounters');
+const createdCountersDiv = document.querySelector('.createdCounters')
 
 //STATEs
 let initalCount = null;
@@ -22,10 +22,46 @@ maxCountValue.addEventListener('input', function(e) {
 })
 
 addCounterButton.addEventListener('click', function(){
-  counters.push({'init':initalCount,
+  const newCounter = {'init':initalCount,
     'max':maxCount
-  });
-  console.log(counters);
+  }
+  counters.push(newCounter);
+  initalCount = 0;
+  maxCount = 0;
+  initalCountValue.value = '';
+  maxCountValue.value = '';
+  
+  const countersBorder = document.createElement("p");
+  const counterInit = document.createElement("p");
+  const buttonIncrement = document.createElement("button");
+  const buttonDecrement = document.createElement("button");
+  buttonDecrement.innerHTML = `Minus`;
+  counterInit.innerHTML = newCounter.init
+  buttonIncrement.innerHTML = `Plus`;
+  countersBorder.innerHTML = `Init:${newCounter.init},  Max:${newCounter.max} <br>`;
+  createdCountersDiv.appendChild(buttonDecrement);
+  createdCountersDiv.appendChild(counterInit);
+  createdCountersDiv.appendChild(buttonIncrement);
+  createdCountersDiv.appendChild(countersBorder);
+
+buttonDecrement.style.height = ('50px');
+buttonDecrement.style.width = ('50px');
+buttonDecrement.style.display = ('inline');
+buttonIncrement.style.height = ('50px');
+buttonIncrement.style.width = ('50px');
+buttonIncrement.style.display = ('inline');
+counterInit.style.display = ('inline');
+countersBorder.style.display = ('inline');
+countersBorder.style.margin = "0px"
+buttonDecrement.style.margin = "10px 10px 10px 10px"
+buttonIncrement.style.margin = "10px 10px 10px 10px"
+counterInit.style.margin = "10px 10px 10px 10px"
+
+  
 });
+
+
+
+
 
 
