71d246c113d4c6da8f545a60f0a4858a318643d2
spahicadis <adis@spahica.com>
2024-08-18 23:31:36 +0200

    Better me

--- app.js
+++ app.js
@@ -1,130 +1,48 @@
-//import { nanoid } from 'nanoid'
-//const uuidv4 = require("uuid/v4");
-//TOMMORW REFACTURING THE CODE AND MAKE IT MORE CLEAN
-
-
-
 //DOM
-const initialCountValue = document.querySelector('.initial-count-js');
-const maxCountValue = document.querySelector('.max-count-js');
-const addCounterButton = document.querySelector('.button-create-js');
-const createdCountersDiv = document.querySelector('.createdCounters');
+const initialCounterValue = document.querySelector('.initial-count-js');
+const maxCounterValue = document.querySelector('.max-count-js');
+const buttonAddCounter = document.querySelector('.button-create-js');
+
 
 //STATEs
-let initialCount = null;
-let maxCount = null;
-let id = 1;
+let initialValue;
+let maxValue;
+let counterID = 0;
 const counters = [];
 
 
 //BINDING
-initialCountValue.addEventListener('input', function(e) {
-  initialCount = parseInt(e.target.value);
-  console.log(initialCount);
+initialCounterValue.addEventListener('input', function(e) {
+  initialValue = parseInt(e.target.value)
+  console.log(initialValue)
 })
 
-maxCountValue.addEventListener('input', function(e) {
-  maxCount = parseInt(e.target.value);
-  console.log(maxCount);
+maxCounterValue.addEventListener('input', function(e) {
+  maxValue = parseInt(e.target.value)
+  console.log(maxValue)
 })
 
-//increment
-
-addCounterButton.addEventListener('click', function(){
-  const newCounter = {'init':initialCount,
-    'max':maxCount,
-    'id':id++,
-    'initBorder':initialCount,
-    'maxBorder':maxCount
-  }
-counters.push(newCounter);
-console.log(newCounter, counters);
-initialCount = 0;
-maxCount = 0;
-initialCountValue.value = '';
-maxCountValue.value = '';
-const countersBorder = document.createElement("p");
-const counterInit = document.createElement("p");
-const buttonIncrement = document.createElement("button");
+function renderCounters() {
 const buttonDecrement = document.createElement("button");
-const info = document.createElement("span");
-buttonDecrement.innerHTML = `Minus`;
-counterInit.innerHTML = newCounter.init
-buttonIncrement.innerHTML = `Plus`;
-countersBorder.innerHTML = `Init:${newCounter.init},  Max:${newCounter.max}`;
-info.innerHTML = `ℹ️ <br>`
-createdCountersDiv.appendChild(buttonDecrement);
-createdCountersDiv.appendChild(counterInit);
-createdCountersDiv.appendChild(buttonIncrement);
-createdCountersDiv.appendChild(countersBorder);
-createdCountersDiv.appendChild(info);
-buttonDecrement.style.height = ('50px');
-buttonDecrement.style.width = ('50px');
-buttonDecrement.style.display = ('inline');
-buttonIncrement.style.height = ('50px');
-buttonIncrement.style.width = ('50px');
-buttonIncrement.style.display = ('inline');
-counterInit.style.display = ('inline');
-countersBorder.style.display = ('inline');
-countersBorder.style.margin = "0px"
-buttonDecrement.style.margin = "10px 10px 10px 10px"
-buttonIncrement.style.margin = "10px 10px 10px 10px"
-counterInit.style.margin = "10px 10px 10px 10px"
-info.style.margin = "10px 10px 10px 10px"
+const buttonIncrement = document.createElement("button");
+const counterCounting = document.createElement("span");
+const counterRange = document.createElement("span");
+const displayCounterInfo = document.createElement("span");
+}
 
-info.addEventListener('click', function() {
-  console.log(counters);
-  console.log(newCounter);
-  console.log(newCounter.id, newCounter.init, newCounter.max);
+buttonAddCounter.addEventListener('click', function() {
+  const counter = {
+    id: counterID++,
+    initial: initialValue,
+    max: maxValue
+  }
+  counters.push(counter);
+  console.log(counters)
+  initialValue = 0;
+  maxValue = 0;
+  console.log(initialValue, maxValue);
+  initialCounterValue.value = "";
+  maxCounterValue.value = "";
+  renderCounters()
 })
-
-function decrement() {
-  initialCount = newCounter.init-1;
-  maxCount = newCounter.max;
-  console.log(initialCount, maxCount);
-  --newCounter.init;
-  counterInit.innerHTML = `${newCounter.init}`;
-  console.log(initialCount, maxCount);
-  console.log(newCounter.initBorder, newCounter.maxBorder);
-  if(initialCount == newCounter.initBorder) {
-    buttonDecrement.removeEventListener('click', decrement);
-  }
-}
-
-function increment() {
-  initialCount = newCounter.init+1;
-  maxCount = newCounter.max;
-  console.log(initialCount, maxCount);
-  ++newCounter.init;
-  counterInit.innerHTML = `${newCounter.init}`;
-  console.log(initialCount);
-  console.log(newCounter.initBorder, newCounter.maxBorder);
-  if(initialCount == newCounter.maxBorder) {
-    buttonIncrement.removeEventListener('click', increment);
-  }
-}
-
-buttonDecrement.addEventListener('click', decrement)
-buttonIncrement.addEventListener('click', increment)
-
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
